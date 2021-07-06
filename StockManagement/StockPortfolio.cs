ab2d2914729a46498f53c1900858439a97dbfe2f
unknown <sivaranjanib99@gmail.com>
2021-07-06 19:20:29 +0530

    [siva] creating the stock

---
+++ StockManagement/StockPortfolio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace StockManagement
+{
+    class StockPortfolio
+    {
+        public  void ReadInput()
+        {
+            StockManager StockManager = new StockManager();
+            string filePath = @"C:\Users\user\source\repos\StockManagement\StockManagement\Stock.Json";
+            StockUtilitty StockUtilitty = JsonConvert.DeserializeObject<StockUtilitty>(File.ReadAllText(filePath));
+            Console.WriteLine("Total share value :{0}", StockManager.PrintReport(StockUtilitty.StockList));
+        }
+    }
+}
---
+++ StockManagement/StockUtilitty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagement
+{
+    class StockUtilitty
+    {
+        public LinkedList<Stock> StockList{ get;  set; }
+
+        public class Stock
+        {
+            public String companyName { get; set; }
+            public  int numberOfShare { get; set; }
+            public  int sharePrice { get; set; }
+        }
+    }
+}
