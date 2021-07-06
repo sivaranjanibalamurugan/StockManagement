da42b698c607565b44719c8da299a90a5ba0200a
unknown <sivaranjanib99@gmail.com>
2021-07-06 12:51:46 +0530

    [siva] creating the stock management

---
+++ StockManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StockManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to stock management!");
+        }
+    }
+}
